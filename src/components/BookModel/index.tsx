c39b547944aaea578898871fd2a85b7428224858
stwalsh4118 <seanwalsh4118@yahoo.com>
2023-04-27 15:34:42 -0500

    add shelf

--- src/components/BookModel/index.tsx
+++ src/components/BookModel/index.tsx
@@ -1,15 +1,27 @@
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import { useFBX } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { useRef, useState } from "react";
 
 const BookModel: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  const fbx = useLoader(GLTFLoader, "/quillbook.glb");
-
+  const fbx = useLoader(GLTFLoader, "/shelf.glb");
+  const mesh = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => {
+    mesh.current.rotation.y += delta;
+  });
   return (
     <>
-      <primitive object={fbx.scene} scale={1.25} rotation={[1.3, 0, 0]} />
+      <primitive
+        ref={mesh}
+        object={fbx.scene}
+        scale={1.25}
+        position={[0, -1, 2]}
+        rotation={[0, 1.6, 0]}
+      />
     </>
   );
 };
