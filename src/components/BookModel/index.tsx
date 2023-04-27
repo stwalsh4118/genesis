c890b0bbd49d385ec7b98eeef07eaecb2bc8f14a
stwalsh4118 <seanwalsh4118@yahoo.com>
2023-04-27 14:54:14 -0500

    fix type

--- src/components/BookModel/index.tsx
+++ src/components/BookModel/index.tsx
@@ -5,10 +5,7 @@ import { useLoader } from "@react-three/fiber";
 
 const BookModel: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  const fbx = useLoader(
-    GLTFLoader,
-    "/quillbook.glb"
-  ) as unknown as THREE.Object3D;
+  const fbx = useLoader(GLTFLoader, "/quillbook.glb");
 
   return (
     <>
